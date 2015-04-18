8262321c4040c73944bb3b17b57dba6850268dde
Ilya Kogan <ilya.kogan@gmail.com>
2015-04-18 17:07:06 +0300

    Refactoring in order to ease debugging

--- android-app/app/src/main/assets/js/nativeApp/nativeAppCallbacks.js
+++ android-app/app/src/main/assets/js/nativeApp/nativeAppCallbacks.js
@@ -1,20 +1,21 @@
 define(function() {
     var callbacks = $.Callbacks();
 
-    function parseDate(msAjaxDate) {
-        return new Date(parseInt(msAjaxDate.replace("/Date(", "").replace(")/",""), 10));
-    }
-
-    function minutesBetween(olderDate, newerDate) {
-        return Math.floor((newerDate - olderDate) / 60000);
-    }
-
     window.onMonitoringInfoArrived = function(stopCodes, monitoringInfo) {
+        visitsByStopCode = initVisitsMap(stopCodes);
+        addVisits(visitsByStopCode, monitoringInfo);
+        fireCallbacks(visitsByStopCode);
+    };    
+
+    function initVisitsMap(stopCodes) {
         visitsByStopCode = [];
         stopCodes.forEach(function(stopCode) {
             visitsByStopCode[stopCode] = [];
         })
+        return visitsByStopCode;
+    }
 
+    function addVisits(visitsByStopCode, monitoringInfo) {
         monitoringInfo.Stops.forEach(function(monitoringStop) {
             var stopCode = monitoringStop.MotiroringRef;
             var visits = [];
@@ -30,12 +31,22 @@ define(function() {
             });
             visitsByStopCode[stopCode] = visits;            
         });
+    }
 
+    function fireCallbacks(visitsByStopCode) {
         for (var stopCode in visitsByStopCode) {
             var visits = visitsByStopCode[stopCode];
             callbacks.fire(stopCode, visits);
-    };
         };
+    }
+
+    function parseDate(msAjaxDate) {
+        return new Date(parseInt(msAjaxDate.replace("/Date(", "").replace(")/",""), 10));
+    }
+
+    function minutesBetween(olderDate, newerDate) {
+        return Math.floor((newerDate - olderDate) / 60000);
+    }
 
     return {
         listen: function(callback) { callbacks.add(callback); }
